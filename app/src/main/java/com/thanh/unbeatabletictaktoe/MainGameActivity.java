b9be85f1fb6e0c4b36a78b8031a0f0bf25917775
Thanh Tran <ttran90@gatech.edu>
2017-01-07 18:41:52 -0500

    different colors when tap

--- app/src/main/java/com/thanh/unbeatabletictaktoe/MainGameActivity.java
+++ app/src/main/java/com/thanh/unbeatabletictaktoe/MainGameActivity.java
@@ -12,6 +12,9 @@ import android.widget.Toast;
 public class MainGameActivity extends AppCompatActivity {
 
     boolean isClicked = false;
+    int[] colors = {Color.RED, Color.BLUE};
+    int count = 0;
+    int currentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,63 +25,81 @@ public class MainGameActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b1.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b1.setBackgroundColor(currentColor);
             }
         });
         final Button b2 = (Button)findViewById(R.id.button2);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b2.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b2.setBackgroundColor(currentColor);
             }
         });
         final Button b3 = (Button)findViewById(R.id.button3);
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b3.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b3.setBackgroundColor(currentColor);
             }
         });
         final Button b4 = (Button)findViewById(R.id.button4);
         b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b4.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b4.setBackgroundColor(currentColor);
             }
         });
         final Button b5 = (Button)findViewById(R.id.button5);
         b5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b5.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b5.setBackgroundColor(currentColor);
             }
         });
         final Button b6 = (Button)findViewById(R.id.button6);
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b6.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b6.setBackgroundColor(currentColor);
             }
         });
         final Button b7 = (Button)findViewById(R.id.button7);
         b7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b7.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b7.setBackgroundColor(currentColor);
             }
         });
         final Button b8 = (Button)findViewById(R.id.button8);
         b8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b8.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b8.setBackgroundColor(currentColor);
             }
         });
         final Button b9 = (Button)findViewById(R.id.button9);
         b9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b9.setBackgroundColor(Color.BLUE);
+                currentColor = colors[count % 2];
+                count++;
+                b9.setBackgroundColor(currentColor);
             }
         });
 
