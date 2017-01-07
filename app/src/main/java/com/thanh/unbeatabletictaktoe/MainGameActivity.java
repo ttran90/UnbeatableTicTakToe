00b50530899277b10208afae3f7c62d7a11717c3
Thanh Tran <ttran90@gatech.edu>
2017-01-07 02:48:15 -0500

    chaged color when click button

--- app/src/main/java/com/thanh/unbeatabletictaktoe/MainGameActivity.java
+++ app/src/main/java/com/thanh/unbeatabletictaktoe/MainGameActivity.java
@@ -1,16 +1,97 @@
 package com.thanh.unbeatabletictaktoe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainGameActivity extends AppCompatActivity {
+
+    boolean isClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        final Button b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b2.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b3 = (Button)findViewById(R.id.button3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b3.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b4 = (Button)findViewById(R.id.button4);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b4.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b5 = (Button)findViewById(R.id.button5);
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b5.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b6 = (Button)findViewById(R.id.button6);
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b6.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b7 = (Button)findViewById(R.id.button7);
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b7.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b8 = (Button)findViewById(R.id.button8);
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b8.setBackgroundColor(Color.BLUE);
+            }
+        });
+        final Button b9 = (Button)findViewById(R.id.button9);
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b9.setBackgroundColor(Color.BLUE);
+            }
+        });
+
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                b1.setBackgroundColor(Color.BLUE);
+//            }
+//        });
+    }
+
+    private void buttonClick(Button button) {
+
     }
 
     @Override
