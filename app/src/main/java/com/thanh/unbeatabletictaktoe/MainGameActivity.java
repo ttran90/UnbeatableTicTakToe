0756004135081f9bfcebab2f08de209f1dd9f943
Thanh Tran <ttran90@gatech.edu>
2017-01-23 00:40:05 -0500

    Implemented onClick to eliminate redudant code

--- app/src/main/java/com/thanh/unbeatabletictaktoe/MainGameActivity.java
+++ app/src/main/java/com/thanh/unbeatabletictaktoe/MainGameActivity.java
@@ -10,9 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainGameActivity extends AppCompatActivity {
+public class MainGameActivity extends AppCompatActivity implements View.OnClickListener{
 
-    boolean isClicked = false;
     int[] colors = {Color.RED, Color.BLUE};
     int count = 0;
     int currentColor;
@@ -22,152 +21,35 @@ public class MainGameActivity extends AppCompatActivity {
     Game game = new Game(player1, player2);
     boolean isOver = false;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
 
-        final TextView textView = (TextView)findViewById(R.id.textBox);
+        TextView textView = (TextView) findViewById(R.id.textBox);
         textView.setText(players[count % 2].getName() + "'s turn");
-        final Button b1 = (Button)findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b1.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 1);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
 
-            }
-        });
-        final Button b2 = (Button)findViewById(R.id.button2);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b2.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 2);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
 
-            }
-        });
-        final Button b3 = (Button)findViewById(R.id.button3);
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b3.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 3);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
-
-            }
-        });
-        final Button b4 = (Button)findViewById(R.id.button4);
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b4.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 4);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
-
-            }
-        });
-        final Button b5 = (Button)findViewById(R.id.button5);
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b5.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 5);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
-
-            }
-        });
-        final Button b6 = (Button)findViewById(R.id.button6);
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b6.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 6);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
-
-            }
-        });
-        final Button b7 = (Button)findViewById(R.id.button7);
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b7.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 7);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
-
-            }
-        });
-        final Button b8 = (Button)findViewById(R.id.button8);
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b8.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 8);
-                count++;
-                //for next player
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
-
-            }
-        });
+        final Button b1 = (Button) findViewById(R.id.button);
+        final Button b2 = (Button) findViewById(R.id.button2);
+        final Button b3 = (Button) findViewById(R.id.button3);
+        final Button b4 = (Button) findViewById(R.id.button4);
+        final Button b5 = (Button) findViewById(R.id.button5);
+        final Button b6 = (Button) findViewById(R.id.button6);
+        final Button b7 = (Button) findViewById(R.id.button7);
+        final Button b8 = (Button) findViewById(R.id.button8);
         final Button b9 = (Button) findViewById(R.id.button9);
-        b9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentColor = colors[count % 2];
-                b9.setBackgroundColor(currentColor);
-                isOver = game.play(players[count % 2], 9);
-                count++;
-                textView.setText(players[count % 2].getName() + "'s turn");
-                if(isOver){
-                    textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
-                }
 
-            }
-        });
-        if(isOver){
-            textView.setText(players[count - 1 % 2].getName().toUpperCase() + " WON!!!!!");
-        }
-
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
     }
 
     @Override
@@ -190,5 +72,51 @@ public class MainGameActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button:
+                buttonClicked((Button)findViewById(R.id.button),1);
+                break;
+            case R.id.button2:
+                buttonClicked((Button)findViewById(R.id.button2),2);
+                break;
+            case R.id.button3:
+                buttonClicked((Button)findViewById(R.id.button3),3);
+                break;
+            case R.id.button4:
+                buttonClicked((Button)findViewById(R.id.button4),4);
+                break;
+            case R.id.button5:
+                buttonClicked((Button)findViewById(R.id.button5),5);
+                break;
+            case R.id.button6:
+                buttonClicked((Button)findViewById(R.id.button6),6);
+                break;
+            case R.id.button7:
+                buttonClicked((Button)findViewById(R.id.button7),7);
+                break;
+            case R.id.button8:
+                buttonClicked((Button)findViewById(R.id.button8),8);
+                break;
+            case R.id.button9:
+                buttonClicked((Button)findViewById(R.id.button9),9);
+                break;
+
+        }
+    }
+
+    private void buttonClicked(Button button, int move) {
+        TextView textView = (TextView)findViewById(R.id.textBox);
+        currentColor = colors[count % 2];
+        button.setBackgroundColor(currentColor);
+        isOver = game.play(players[count % 2], move);
+        count++;
+        textView.setText(players[count % 2].getName() + "'s turn");
+        if(isOver){
+            textView.setText(players[(count - 1) % 2].getName().toUpperCase() + " WON!!!!!");
+        }
     }
 }
